19cfc0122335abeeb28f84196798b7631023fbe2
mikrob <mikael.robert@botsunit.com>
2016-10-12 19:30:50 +0200

    format code

--- main.go
+++ main.go
@@ -1,35 +1,32 @@
 package main
 
-
 // exemple of usage :
 // go run main.go -c ~/.kube/config -k pod -n etcd0 -ns default
 
-
 import (
-  "flag"
-  "fmt"
-  "errors"
-  "time"
-  "k8s.io/client-go/1.4/kubernetes"
-  "k8s.io/client-go/1.4/tools/clientcmd"
-//  "k8s.io/client-go/1.4/pkg/api/v1"
+	"errors"
+	"flag"
+	"fmt"
+	"k8s.io/client-go/1.4/kubernetes"
+	"k8s.io/client-go/1.4/tools/clientcmd"
+	"time"
+	//  "k8s.io/client-go/1.4/pkg/api/v1"
 )
 
 var (
-  kubeconfig = flag.String("c", "./config", "absolute path to the kubeconfig file")
-  resource_kind = flag.String("k", "rc", "resource kind name : pod, svc, rc, petset ...")
-  resource_name = flag.String("n", "mysuperpod", "resource name e.g. pod name, rc name ...")
-  namespace = flag.String("ns", "namespace", "namespace of the resource")
+	kubeconfig    = flag.String("c", "./config", "absolute path to the kubeconfig file")
+	resource_kind = flag.String("k", "rc", "resource kind name : pod, svc, rc, petset ...")
+	resource_name = flag.String("n", "mysuperpod", "resource name e.g. pod name, rc name ...")
+	namespace     = flag.String("ns", "namespace", "namespace of the resource")
 )
 
-
-func waitPod(clientset * kubernetes.Clientset, namespace string, podname string) (bool, error) {
+func waitPod(clientset *kubernetes.Clientset, namespace string, podname string) (bool, error) {
 	timeout := time.After(15 * time.Second)
 	tick := time.Tick(2 * time.Second)
 	for {
 		select {
 		case <-timeout:
-                        msg := fmt.Sprintf("Timeout after %d seconds", 15)
+			msg := fmt.Sprintf("Timeout after %d seconds", 15)
 			return false, errors.New(msg)
 		case <-tick:
 			ok, err := podRunning(clientset, namespace, podname)
@@ -42,38 +39,36 @@ func waitPod(clientset * kubernetes.Clientset, namespace string, podname string)
 	}
 }
 
-
-func podRunning(clientset * kubernetes.Clientset, namespace string, podname string) (bool, error) {
-  pod, err := clientset.Core().Pods(namespace).Get(podname)
-  fmt.Println("Namespace :", namespace)
-  fmt.Println("Pod:", podname)
-  if err != nil {
-    panic(err.Error())
-  }
-  state := pod.Status.Phase
-  //fmt.Printf("%+v\n", status)
-  fmt.Println("Status :", state)
-  return state == "Prout", err
+func podRunning(clientset *kubernetes.Clientset, namespace string, podname string) (bool, error) {
+	pod, err := clientset.Core().Pods(namespace).Get(podname)
+	fmt.Println("Namespace :", namespace)
+	fmt.Println("Pod:", podname)
+	if err != nil {
+		panic(err.Error())
+	}
+	state := pod.Status.Phase
+	//fmt.Printf("%+v\n", status)
+	fmt.Println("Status :", state)
+	return state == "Prout", err
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Println("Resource Kind is :", *resource_kind)
-  fmt.Println("Resource Name is : ", *resource_name)
-  fmt.Println("Namespace is : ", *namespace)
-  config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-  if err != nil {
-    panic(err.Error())
-  }
-  clientset, err := kubernetes.NewForConfig(config)
-  if err != nil {
-    panic(err.Error())
-  }
+	fmt.Println("Resource Kind is :", *resource_kind)
+	fmt.Println("Resource Name is : ", *resource_name)
+	fmt.Println("Namespace is : ", *namespace)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  _, pod_err := waitPod(clientset, *namespace, *resource_name)
-  if pod_err != nil {
-	panic(pod_err.Error())
-  }
+	_, pod_err := waitPod(clientset, *namespace, *resource_name)
+	if pod_err != nil {
+		panic(pod_err.Error())
+	}
 }
-
