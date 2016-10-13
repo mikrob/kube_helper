fd4e2433e8ea7a1d1b7cdf68f8d38941a9b2f078
mikrob <mikael.robert@botsunit.com>
2016-10-13 12:22:31 +0200

    huge refactoring to make polymorphism between resource kind

--- main.go
+++ main.go
@@ -7,42 +7,50 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"reflect"
+	"time"
+
 	"k8s.io/client-go/1.4/kubernetes"
 	"k8s.io/client-go/1.4/tools/clientcmd"
-	"time"
-	//  "k8s.io/client-go/1.4/pkg/api/v1"
 )
 
 var (
-	kubeconfig    = flag.String("c", "./config", "absolute path to the kubeconfig file")
-	resource_kind = flag.String("k", "rc", "resource kind name : pod, svc, rc, petset ...")
-	resource_name = flag.String("n", "mysuperpod", "resource name e.g. pod name, rc name ...")
-	namespace     = flag.String("ns", "namespace", "namespace of the resource")
+	kubeconfig     = flag.String("c", "./config", "absolute path to the kubeconfig file")
+	resourceKind   = flag.String("k", "rc", "resource kind name : pod, svc, rc, petset ...")
+	resourceName   = flag.String("n", "mysuperpod", "resource name e.g. pod name, rc name ...")
+	namespace      = flag.String("ns", "namespace", "namespace of the resource")
+	resourceMapper = map[string]reflect.Type{
+		"pod": reflect.TypeOf(PodCheckStatus{}),
+		"rc":  reflect.TypeOf(ReplicationControllerCheckStatus{}),
+	}
 )
 
-func waitPod(clientset *kubernetes.Clientset, namespace string, podname string) (bool, error) {
-	timeout := time.After(15 * time.Second)
-	tick := time.Tick(2 * time.Second)
-	for {
-		select {
-		case <-timeout:
-			msg := fmt.Sprintf("Timeout after %d seconds", 15)
-			return false, errors.New(msg)
-		case <-tick:
-			ok, err := podRunning(clientset, namespace, podname)
-			if err != nil {
-				return false, err
-			} else if ok {
-				return true, nil
-			}
-		}
-	}
+// KubeResource is a generic type to define a Kubernetes Resources
+type KubeResource interface {
+	checkState()
+	new(clientSet kubernetes.Clientset, namespace string, name string)
 }
 
-func podRunning(clientset *kubernetes.Clientset, namespace string, podname string) (bool, error) {
-	pod, err := clientset.Core().Pods(namespace).Get(podname)
-	fmt.Println("Namespace :", namespace)
-	fmt.Println("Pod:", podname)
+// PodCheckStatus is our internal representation of pod
+type PodCheckStatus struct {
+	clientSet *kubernetes.Clientset
+	namespace string
+	name      string
+}
+
+// ReplicationControllerCheckStatus is our internal representation of replictionController
+type ReplicationControllerCheckStatus struct{}
+
+func (p *PodCheckStatus) new(clientSet kubernetes.Clientset, namespace string, name string) {
+	p.clientSet = &clientSet
+	p.namespace = namespace
+	p.name = name
+}
+
+func (p *PodCheckStatus) checkState() (bool, error) {
+	pod, err := p.clientSet.Core().Pods(p.namespace).Get(p.name)
+	fmt.Println("Namespace :", p.namespace)
+	fmt.Println("Pod:", p.name)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -52,11 +60,36 @@ func podRunning(clientset *kubernetes.Clientset, namespace string, podname strin
 	return state == "Prout", err
 }
 
+func waitResource(resource *KubeResource) (bool, error) {
+	timeout := time.After(15 * time.Second)
+	tick := time.Tick(2 * time.Second)
+	for {
+		select {
+		case <-timeout:
+			msg := fmt.Sprintf("Timeout after %d seconds", 15)
+			return false, errors.New(msg)
+		case <-tick:
+			ok, err := resource.checkState()
+			if err != nil {
+				return false, err
+			} else if ok {
+				return true, nil
+			}
+		}
+	}
+}
+
+func makeInstance(name string) interface{} {
+	v := reflect.New(resourceMapper[name]).Elem()
+	// Maybe fill in fields here if necessary
+	return v.Interface()
+}
+
 func main() {
 	flag.Parse()
 
-	fmt.Println("Resource Kind is :", *resource_kind)
-	fmt.Println("Resource Name is : ", *resource_name)
+	fmt.Println("Resource Kind is :", *resourceKind)
+	fmt.Println("Resource Name is : ", *resourceName)
 	fmt.Println("Namespace is : ", *namespace)
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
@@ -66,9 +99,16 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+	var kubeResource *KubeResource
+	if _, ok := resourceMapper[*resourceKind]; ok {
+		kubeResource := makeInstance(*resourceKind)
+		kubeResource.new(clientset, *namespace, *resourceName)
+	} else {
+		panic(fmt.Sprintf("Dont known what to do with kind : %s", *resourceKind))
+	}
 
-	_, pod_err := waitPod(clientset, *namespace, *resource_name)
-	if pod_err != nil {
-		panic(pod_err.Error())
+	_, podErr := waitResource(kubeResource)
+	if podErr != nil {
+		panic(podErr.Error())
 	}
 }
