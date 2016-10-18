d28865b50f232cb0a1b43a6ede30c9e9c6842e2a
mikrob <mikael.robert@botsunit.com>
2016-10-18 13:26:16 +0200

    wait petset fully functionnal

--- model/main.go
+++ model/main.go
@@ -2,8 +2,10 @@ package model
 
 import (
 	"fmt"
+	"regexp"
 
 	"k8s.io/client-go/1.4/kubernetes"
+	"k8s.io/client-go/1.4/pkg/api"
 )
 
 // KubeResource is an abstract representation of kuberesource
@@ -29,14 +31,49 @@ type PetSet struct {
 	Name      string
 }
 
+func allPredicate(vs []bool, f func(bool) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
 // CheckState for PetSet
 func (ps PetSet) CheckState(clientSet *kubernetes.Clientset) (bool, error) {
-	petset, err := clientSet.Apps().PetSets(ps.Namespace).Get(ps.Name)
+	petSet, err := clientSet.Apps().PetSets(ps.Namespace).Get(ps.Name)
+
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("%+v\n", petset)
-	return true, nil
+	wantedReplicas := petSet.Spec.Replicas
+	psStatusReplicas := petSet.Status.Replicas
+	fmt.Println("Wanted replicas : ", *wantedReplicas)
+	fmt.Println("Actual Replicas :", psStatusReplicas)
+
+	petSetPods, errPods := clientSet.Core().Pods(ps.Namespace).List(api.ListOptions{})
+	if errPods != nil {
+		panic(errPods.Error())
+	}
+	//fmt.Println("TYPE :", reflect.TypeOf(petSetPods.Items))
+	// fmt.Printf("%+v\n", petSetPods.Items())
+	var podReadys []bool
+	for _, pod := range petSetPods.Items {
+		pName := string(pod.Name)
+		match, _ := regexp.MatchString(ps.Name, pName)
+		if match {
+			fmt.Println(fmt.Sprintf("Pod : %s, Status : %s", pName, pod.Status.Phase))
+			podReadys = append(podReadys, pod.Status.Phase == "Running")
+		}
+	}
+
+	//podReadys = append(podReadys, false)
+	allReady := allPredicate(podReadys, func(v bool) bool {
+		return v
+	})
+
+	return allReady && *wantedReplicas == psStatusReplicas, nil
 }
 
 // CheckState for pod
