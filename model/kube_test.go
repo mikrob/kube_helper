1835e316c9ee9120826aed78d989d0f29f60461d
mikrob <mikael.robert@botsunit.com>
2016-10-19 16:45:43 +0200

    add tests

--- model/kube_test.go
+++ model/kube_test.go
@@ -1,11 +1,38 @@
 package model
 
-import "testing"
+import (
+	"testing"
 
-func TestKube(t *testing.T) {
-	expected := "Hello Go!"
-	actual := "Hello"
-	if actual != expected {
+	fakeclientset "k8s.io/client-go/1.4/kubernetes/fake"
+)
+
+func TestAllPredicateTrue(t *testing.T) {
+	boolArray := []bool{true, true, true}
+	allReady := allPredicate(boolArray, func(v bool) bool {
+		return v
+	})
+	if !allReady {
+		t.Error("Test failed")
+	}
+}
+
+func TestAllPredicateWithOneFalse(t *testing.T) {
+	boolArray := []bool{true, true, false}
+	allReady := allPredicate(boolArray, func(v bool) bool {
+		return v
+	})
+	if allReady {
+		t.Error("Test failed, should be false")
+	}
+}
+
+func TestCheckService(t *testing.T) {
+	kubeResource := Service{Name: "myservice", Namespace: "mynamespace"}
+	clientSet := fakeclientset.NewSimpleClientset()
+	result, resultErr := kubeResource.CheckState(clientSet)
+	t.Log("prout")
+	t.Logf("%+v\n", result)
+	if resultErr != nil {
 		t.Error("Test failed")
 	}
 }
